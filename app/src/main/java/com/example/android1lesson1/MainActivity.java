fa9c86070edb0ccb15154a18e9f84d1fce075219
Aiperi Ruslanbekova <aruslanbekova1@gmail.com>
2021-04-19 02:46:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android1lesson1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android1lesson1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+        }
